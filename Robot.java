50a07462c168afdd0681f7865a3711745208f273
Will <willyingling@gmail.com>
2015-01-06 15:02:28 -0700

    initial commit

---
+++ Robot.java
@@ -0,0 +1,23 @@
+
+package org.usfirst.frc.team662.robot;
+
+import edu.wpi.first.wpilibj.SampleRobot;
+
+public class Robot extends SampleRobot {
+
+
+    public Robot() {
+    	
+    }
+
+    public void autonomous() {
+    
+    }
+
+    public void operatorControl() {
+        
+    }
+
+    public void test() {
+    }
+}
