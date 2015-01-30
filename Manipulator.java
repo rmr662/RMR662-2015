b715e30f51b360a71320884da6080376b059aa9e
Drive Station Laptop <danielmercer1@gmail.com>
2015-01-29 20:07:21 -0700

    First successful merge

--- Manipulator.java
+++ Manipulator.java
@@ -1,16 +1,49 @@
 package org.usfirst.frc.team662.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class Manipulator extends Component {
-
-	public Manipulator(Joystick j) {
+	
+	public static final int BRAKE_PORT = 0;
+	public static final int LIFT_PORT = 0;
+	public static final double BRAKE_VAL = 0.5;
+	public static final double DEAD_ZONE = 0.1;
+	
+	Talon liftMotor;
+	Solenoid airBrake;
+	Joystick xbox;
+	
+	double liftVal;
+	
+	public Manipulator() {
+		xbox = new Joystick(0);
+		liftMotor = new Talon(LIFT_PORT);
+		//airBrake = new Solenoid(BRAKE_PORT);
+		
+		//airBrake.set(false);
+		liftVal = 0.0;
+		liftMotor.setSafetyEnabled(true);
+		liftMotor.setExpiration(.5);
 		
 	}
 	
 	public void update() {
-		// TODO Auto-generated method stub
+		liftVal = xbox.getRawAxis(XboxMap.LEFT_JOY_VERT);
+	
+		if(Math.abs(liftVal) < DEAD_ZONE){
+			liftVal = 0;
+		}
 		
+		liftMotor.set(liftVal);
+		
+		/*if(xbox.getRawAxis(XboxMap.RIGHT_TRIGGER) <= BRAKE_VAL){
+			airBrake.set(true);
+		}
+		else{
+			airBrake.set(false);
+		}*/
 	}
 
 }
--- Robot.java
+++ Robot.java
@@ -1,25 +1,45 @@
-
 package org.usfirst.frc.team662.robot;
+
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SampleRobot;
 
 public class Robot extends SampleRobot {
 
-	Drive d;
-	Manipulator m;
+	final int DRIVE_INDEX = 0;
+	final int MANIP_INDEX = 1;
+	final int NUM_COMPONENTS = 2;
+	
+	Component [] parts;
+	
 	Joystick xbox;
 	
     public Robot() {
     	
+    	parts = new Component[NUM_COMPONENTS];
+    	
+    	//parts[0] = new Drive();
+    	parts[1] = new Manipulator();
+    	
     }
 
     public void autonomous() {
-    	Autonomous.run(d, m);
+    	Autonomous.run((Drive)parts[DRIVE_INDEX], (Manipulator)parts[MANIP_INDEX]);
     }
+
 
     public void operatorControl() {
         
+    	while (isEnabled() && isOperatorControl()) {
+    		for (int c = 0; c < parts.length; c++) {
+    			if (parts[c] != null)
+    			{
+    				parts[c].update();
+    			}
+    		
+    		}
+    	}
+    	
     }
 
     public void test() {
