8aad8f46af3e40cba5a09a09cf3bbc8c33f73342
Drive Station Laptop <danielmercer1@gmail.com>
2015-01-31 13:38:00 -0700

    Updated drive and manipulator

--- Robot.java
+++ Robot.java
@@ -11,45 +11,47 @@ public class Robot extends SampleRobot {
 	final int MANIP_INDEX = 1;
 	final int NUM_COMPONENTS = 2;
 
-	// Array of parts. It's an array in case we want to add things like an LED controller
-    	Component [] parts;
+	// Array of parts. It's an array in case we want to add things like an LED
+	// controller
+	Component[] parts;
 
-    public Robot() {
+	public Robot() {
 
-    	parts = new Component[NUM_COMPONENTS];
+		parts = new Component[NUM_COMPONENTS];
 
-	// Comment out either of these to disable parts of the robot
-    	//parts[DRIVE_INDEX] = new Drive();
-    	parts[MANIP_INDEX] = new Manipulator();
+		// Comment out either of these to disable parts of the robot
+		// parts[DRIVE_INDEX] = new Drive();
+		parts[MANIP_INDEX] = new Manipulator();
 
-    }
+	}
 
-    // This method gives a Drive object and a Manipulator object to control
-    public void autonomous() {
-    	Autonomous.run((Drive)parts[DRIVE_INDEX], (Manipulator)parts[MANIP_INDEX]);
-    }
+	// This method gives a Drive object and a Manipulator object to control
+	public void autonomous() {
+		Autonomous.run((Drive) parts[DRIVE_INDEX],
+				(Manipulator) parts[MANIP_INDEX]);
+	}
 
 	// Main control method of the robot
-    public void operatorControl() {
+	public void operatorControl() {
 
-	// Continuous loop for operator control. Don't change this
-    	while (isEnabled() && isOperatorControl()) {
+		// Continuous loop for operator control. Don't change this
+		while (isEnabled() && isOperatorControl()) {
 
-		// Cycles through the parts, updating them all
-    		for (int c = 0; c < parts.length; c++) {
-			// This block allows for disabling parts of the robot without breaking the code
-    			if (parts[c] != null)
-    			{
-    				parts[c].update();
-    			}
+			// Cycles through the parts, updating them all
+			for (int c = 0; c < parts.length; c++) {
+				// This block allows for disabling parts of the robot without
+				// breaking the code
+				if (parts[c] != null) {
+					parts[c].update();
+				}
 
-    		}
-    	}
+			}
+		}
 
-    } // End of operatorControl
+	} // End of operatorControl
 
 	// This method isn't used
-    public void test() {
-    }
+	public void test() {
+	}
 
 } // End class Robot
